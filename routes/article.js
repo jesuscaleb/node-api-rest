708c55d51246877df348531a45560da6371dd3f1
jesuscaleb <almostperfect557@gmail.com>
2021-01-07 17:26:41 -0500

    Handling size of output values in functions

--- routes/article.js
+++ routes/article.js
@@ -15,9 +15,8 @@ const uniqueSuffix = () => {
   let date = ("0" + now.getDate()).slice(-2);
   let month = ("0" + (now.getMonth() + 1)).slice(-2);
   let year = now.getFullYear();
-  let inline_now = date + month + year;
-  let output = inline_now + Math.round(Math.random() * 1E9);
-
+  let output = date + month + year;
+  
   return output;
 }
 
